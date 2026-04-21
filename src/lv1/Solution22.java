50086e887c6877e527ff291622dea1a21a480c9f
정예진 <yxejxnn@gmail.com>
2026-04-21 09:37:07 +0900

    22. 두 정수 사이의 합

---
+++ src/lv1/Solution22.java
@@ -0,0 +1,15 @@
+package lv1;
+
+public class Solution22 {
+    public long solution(int a, int b) {
+        long answer = 0;
+
+        int start = Math.min(a, b);
+        int end = Math.max(a, b);
+
+        for (int i = start; i <= end; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
