c681591dd649be684ff0d35fa68c08894e0f1178
정예진 <yxejxnn@gmail.com>
2026-04-21 09:09:21 +0900

    21. 하샤드 수

---
+++ src/lv1/Solution21.java
@@ -0,0 +1,17 @@
+package lv1;
+
+public class Solution21 {
+    public boolean solution(int x) {
+        int sum = 0;
+        int temp = x;
+
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        if (x % sum == 0) {
+            return true;
+        }
+        return false;
+    }
+}
