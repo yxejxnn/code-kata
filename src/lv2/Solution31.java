5ef5465d005948542b69b57004a912ed55fdf2dc
정예진 <yxejxnn@gmail.com>
2026-04-27 09:35:43 +0900

    31. 수박수박수박수박수박수?

---
+++ src/lv2/Solution31.java
@@ -0,0 +1,17 @@
+package lv2;
+
+public class Solution31 {
+     public String solution(int n) {
+         String answer = "";
+
+         for (int i = 1; i <= n; i++) {
+             if (i % 2 == 1) {
+                 answer += "수";
+             } else {
+                 answer += "박";
+             }
+         }
+
+         return answer;
+    }
+}
