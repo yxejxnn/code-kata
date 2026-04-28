d2a380dafd34f98db99c746dea31303a20c26740
정예진 <yxejxnn@gmail.com>
2026-04-28 09:43:30 +0900

    32. 내적

---
+++ src/lv2/Solution32.java
@@ -0,0 +1,13 @@
+package lv2;
+
+public class Solution32 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+}
